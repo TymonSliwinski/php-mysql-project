342b06176b038eaec06fd24f30b43acf9b36e3c8
Tymon Śliwiński <tymon.sliwinski24@gmail.com>
2023-05-16 16:41:15 +0200

    initial commit

---
+++ src/php/index.php
@@ -0,0 +1,12 @@
+
+<?php
+$user = $_ENV['DB_USER'] || 'root';
+$pass = $_ENV['DB_PASSWORD'];
+
+try {
+    $dbh = new PDO('mysql:host=db;port=3306;dbname=public', $user, $pass);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
