5bcf2ac6080042145d594c0873dcb960c69296df
Tymon Śliwiński <tymon.sliwinski24@gmail.com>
2023-05-24 22:43:13 +0200

    feat(db): add init file, fix(compose): build problems

--- src/php/index.php
+++ src/php/index.php
@@ -1,10 +1,15 @@
 
 <?php
+$db_name = $_ENV['DB_NAME'] ? $_ENV['DB_NAME'] : 'public';
 $user = $_ENV['DB_USER'] ? $_ENV['DB_USER'] : 'root';
 $pass = $_ENV['DB_PASSWORD'];
 
 try {
-    $dbh = new PDO('mysql:host=db;port=3306;dbname=public', $user, $pass);
+    $dbh = new PDO("mysql:host=db;port=3306;dbname=$db_name", $user, $pass);
+    echo "Connected to DB!";
+    echo "<br/>";
+    print_r($dbh->query('SHOW TABLES')->fetchAll());
+
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
